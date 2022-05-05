fa1caf59ff260df0d1dda4befe03cc82179af52c
LeppoXena <francesco.lepore@xenagames.com>
2022-05-05 18:52:57 +0200

    Details

--- js/mysript.js
+++ js/mysript.js
@@ -6,15 +6,18 @@ const intoSection=document.querySelector("section.container");
 
 for (let i=1; i<101; i++) {
     let myAdd;
+
     if (i%15==0) {
         myAdd=`<div class="square fizz buzz"><h5>FizzBuzz!</h5></div>`}
+
     else if (i%3==0) {
         myAdd=`<div class="square fizz"><h5>Fizz</h5></div>`}
+    
     else if (i%5==0) {
         myAdd=`<div class="square buzz"><h5>Buzz</h5></div>`}
+    
     else {
         myAdd=`<div class="square"><h5> ${i} </h5></div>`}
 
-        intoSection.innerHTML += myAdd;
-
-    }
+    intoSection.innerHTML += myAdd;
+}
